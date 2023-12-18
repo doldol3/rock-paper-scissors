098795e9a55aae212c4f34371b342a2511728b30
Jun Park <125324779+doldol3@users.noreply.github.com>
2023-12-19 08:38:49 +1300

    Remove all console.log methods in playRound function

--- script.js
+++ script.js
@@ -14,17 +14,15 @@ getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log('Draw');
         return 'draw';
     } else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'paper') ||
         (playerSelection === 'paper' && computerSelection === 'rock')
     ) {
-        console.log('You Win!');
         return 'win';
     } else {
-        console.log('You Lose!');
         return 'lose';
     }
 }
+

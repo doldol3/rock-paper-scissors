e37b9d0669244036186f5965b696445b372d3810
Jun Park <125324779+doldol3@users.noreply.github.com>
2023-12-18 18:41:18 +1300

    Improve playRound function
    Tidy up conditional statements. Account for TIES by re-playing the round.

--- script.js
+++ script.js
@@ -13,19 +13,18 @@ function getComputerChoice() {
 getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log('You Lose!')
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log('You Win!')
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log('You Lose!')
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log('You Win!')
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log('You Lose!')
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log('You Win!')
-    } else if (playerSelection == computerSelection) {
-        console.log('Draw')
+    if (playerSelection === computerSelection) {
+        console.log('Draw');
+        return 'draw';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+        console.log('You Win!');
+        return 'win';
+    } else {
+        console.log('You Lose!');
+        return 'lose';
     }
 }
